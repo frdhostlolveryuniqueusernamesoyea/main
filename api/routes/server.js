4f3c184e298eff8c6e2770e63114ebec026463ef
jakub-petrovic <77661436+jakub-petrovic@users.noreply.github.com>
2024-10-15 19:48:18 +0200

    posh push

--- api/routes/server.js
+++ api/routes/server.js
@@ -5,6 +5,10 @@ const app = express();
 // Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '..', '..')));
 
+app.get('/favicon.webp', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', '..', 'favicon.webp'));
+});
+
 app.get('/q&a', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'pages', 'q&a.html'));
 });
