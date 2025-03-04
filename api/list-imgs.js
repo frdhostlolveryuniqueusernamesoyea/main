8af48dbfa5cf11a3369ee64e4375bf21c9b5d230
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-03-04 14:52:34 +0100

    s

--- api/list-imgs.js
+++ api/list-imgs.js
@@ -2,10 +2,11 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-    const directoryPath = path.resolve('/images'); // Change this if needed
+    const directoryPath = path.join("/", 'images'); 
+
     fs.readdir(directoryPath, (err, files) => {
         if (err) {
-            return res.status(500).json({ error: 'Unable to list directory' });
+            return res.status(500).json({ error: 'Unable to list directory', details: err.message });
         }
         res.status(200).json({ files });
     });
