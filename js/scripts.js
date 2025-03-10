332f078d4371de267d5675f56f98e7f79947ff45
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-03-10 14:53:39 +0100

    does it work? (language pack 1st push)

--- js/scripts.js
+++ js/scripts.js
@@ -203,3 +203,8 @@ function openCenteredWindow(url, title = "Contact Window | FrDomains", width = "
     window.open(url, title, features);
 }
 
+
+// language pack
+const langpack_name = document.getElementById('langpackbox').value;
+//const langpack = fetch('/lang/' + langpack_name);
+document.getElementById('bodej').innerText = langpack_name;
