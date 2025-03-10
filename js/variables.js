595b22188f9c94949344e17d85d1b211ef3376f5
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-03-10 19:22:18 +0100

    fix warning , i forgor that i //

--- js/variables.js
+++ js/variables.js
@@ -72,7 +72,7 @@ const navbar = `                <!-- Text Logo - Use this if you don't have a gr
     fetch('https://frdomains.site/pages/warning.html')
       .then(response => response.text())
       .then(data => {
-        //document.getElementById('warning').innerHTML = data;
+        document.getElementById('warning').innerHTML = data;
       })
       .catch(error => {
         console.error('Error:', error);
