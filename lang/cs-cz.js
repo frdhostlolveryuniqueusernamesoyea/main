dfb03f4f14820f30374b2e0a838d3db82b18324a
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-03-10 19:59:17 +0100

    users counter and subdomain translate & pricing mainrtext & subtext

--- lang/cs-cz.js
+++ lang/cs-cz.js
@@ -1,3 +1,8 @@
 document.getElementById('getfreesubdomain').innerText = "Získej zdarma subdoménu s (brzy) plným přístupem";
 document.getElementById('get').innerText = "Získat";
 document.getElementById('pricinkbbtn').innerText = "Plány";
+document.getElementById('userslbl').innerText = "Uživatelů";
+document.getElementById('subdomainsorderd').innerText = "Objednáno subdomén";
+document.getElementById('subdomainsplas').innerText = "Z toho prémiových";
+document.getElementById('pricingmaintext').innerText = "Plány";
+document.getElementById('pricingsubtext').innerText = "Plas je levné že?";
--- lang/en-us.js
+++ lang/en-us.js
@@ -1,3 +1,6 @@
 document.getElementById('getfreesubdomain').innerText = "Get a free subdomain with (soon) full access";
 document.getElementById('get').innerText = "Get";
 document.getElementById('pricinkbbtn').innerText = "Pricing";
+document.getElementById('userslbl').innerText = "Users";
+document.getElementById('subdomainsorderd').innerText = "Subdomains ordered";
+document.getElementById('subdomainsplas').innerText = "Of which Plas";
