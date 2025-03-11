ece3f6fbbc0670f57b4ef26f81efb2cbd932129c
MrTomiCZ <131264466+MrTomiCZ@users.noreply.github.com>
2025-03-11 20:05:34 +0100

    make standard translate

--- lang/en-us.js
+++ lang/en-us.js
@@ -10,13 +10,15 @@ document.getElementById('subdomainsplas').innerText = "Of which Plas";
 
 document.getElementById('pricingmaintext').innerText = "Pricing";
 document.getElementById('pricingsubtext').innerText = "Plas is cheap right?";
+
 document.getElementById('semesterplas').innerText = "every semester";
 document.getElementById('halfyearplas').innerText = "(half a year)";
 document.getElementById('plas-subtext').innerText = "sometimes called as frdomains plus or premium";
-
 document.getElementById('plas-text-1').innerText = "Multiple email addresses";
 document.getElementById('plas-text-2').innerText = "Multiple subdomains";
 document.getElementById('plas-text-moredomains').innerText = "+ more domains (none yet)";
 
+document.getElementById('lifetimestandard').innerText = "lifetime";
+document.getElementById('standard-subtext').innerText = "frdomains free - pretty much everything thats in plas but a little less";
 document.getElementById('standard-text-1').innerText = "Multiple email addresses";
 document.getElementById('standard-text-2').innerText = "Multiple subdomains";
